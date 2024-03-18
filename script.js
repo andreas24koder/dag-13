d8089882a01e52ffff15dafd43040dc9c2d438f7
andreas24koder <andreas24.koder@gmail.com>
2024-03-18 13:16:48 +0100

    First commit

---
+++ script.js
@@ -0,0 +1,16 @@
+console.log("Hi planet eart!");
+
+//datetype: nummber
+let age = 23;
+console.log(typeof age)
+
+//datatype: string
+const firstname = "Andreas";
+console.log(typeof firstname)
+
+//datatype: booloen
+let isonline = true;
+console.log(tyoeof isOnline)
+
+cost sentence = age + firstname + isonline:
+console.log(sentence)
